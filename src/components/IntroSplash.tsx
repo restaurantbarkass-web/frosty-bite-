d6469f129df9ee705b7057c303630415a9c3371d
restaurantbarkass-web <restaurantbarkass@gmail.com>
2026-05-17 17:40:28 +0530

    feat: Adjust intro splash opacity
    Reduce the opacity of the year overlay in the intro splash screen for a more subtle visual effect.

--- src/components/IntroSplash.tsx
+++ src/components/IntroSplash.tsx
@@ -91,7 +91,7 @@ export const IntroSplash: React.FC<IntroSplashProps> = ({ onComplete }) => {
       {/* Decorative Branding */}
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 0.10 }}
+        animate={{ opacity: 0.05 }}
         className="absolute bottom-10 left-10 text-white font-black text-8xl pointer-events-none select-none"
       >
         2026
