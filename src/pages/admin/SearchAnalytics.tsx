3d464c1213cf02c091d57312f52dd0f9ce3260e4
restaurantbarkass-web <restaurantbarkass@gmail.com>
2026-08-14 18:03:07 +0530

    error fixed !!

--- src/pages/admin/SearchAnalytics.tsx
+++ src/pages/admin/SearchAnalytics.tsx
@@ -168,7 +168,7 @@ export const SearchAnalytics: React.FC = () => {
             </h3>
           </div>
           <div className="h-[400px] w-full">
-            <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={0} debounce={100}>
               <AreaChart data={chartData}>
                 <defs>
                   <linearGradient id="colorSearches" x1="0" y1="0" x2="0" y2="1">
